afe07e2e6b03e0d35b002396c4662c41d6ebcaba
Joan Pou Ramon <joanp2001@gmail.com>
2022-04-11 11:53:09 +0200

    Added "final" statments to Board class

--- src/Board.java
+++ src/Board.java
@@ -1,9 +1,9 @@
 public class Board {
-    private static int[][] board = new int[7][8];
-    private static String p1 = "\uD83D\uDD34 ";
-    private static String p2 = "\uD83D\uDD35 ";
-    private static String blank = "\uD83C\uDF19 ";
-    private static String border = "\uD83D\uDFE8 ";
+    private static final int[][] board = new int[7][8];
+    private static final String p1 = "\uD83D\uDD34 ";
+    private static final String p2 = "\uD83D\uDD35 ";
+    private static final String blank = "\uD83C\uDF19 ";
+    private static final String border = "\uD83D\uDFE8 ";
 
     public static void printBoard() {
         System.out.println("[1] [2] [3] [4] [5] [6] [7] [8]");
