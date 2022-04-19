1af8f008c42cdc1db8c51677ed938396a19fe0d3
Joan Pou Ramon <joanp2001@gmail.com>
2022-04-19 16:26:08 +0200

    Fixed board size

--- src/Board.java
+++ src/Board.java
@@ -1,18 +1,22 @@
 public class Board {
-    private static final int[][] board = new int[7][8];
+    private static final int[][] board = new int[7][6];
     private static final String p1 = "\uD83D\uDD34 ";
     private static final String p2 = "\uD83D\uDD35 ";
     private static final String blank = "\uD83C\uDF19 ";
     private static final String border = "\uD83D\uDFE8 ";
 
     public static void printBoard() {
-        System.out.println("[1] [2] [3] [4] [5] [6] [7] [8]");
-        for (int i = 0; i < 7; i++) {
+        System.out.print("   ");
+        for (int i = 1; i <= board.length; i++) {
+            System.out.print("[" + i + "]");
+        }
+        System.out.println();
+        for (int y = 0; y < board[0].length; y++) {
             System.out.print(border);
-            for (int j = 0; j < 8; j++) {
-                if (Board.board[i][j] == 0) {
+            for (int x = 0; x < board.length; x++) {
+                if (Board.board[x][y] == 0) {
                     System.out.print(blank);
-                } else if (Board.board[i][j] == 1) {
+                } else if (Board.board[x][y] == 1) {
                     System.out.print(p1);
                 } else {
                     System.out.print(p2);
@@ -20,6 +24,6 @@ public class Board {
             }
             System.out.println(border);
         }
-        System.out.println(border.repeat(10));
+        System.out.println(border.repeat(board.length+2));
     }
 }
